125e27fb302a6746728dbbe2a5544e63982f9c6b
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2024-02-07 19:40:12 +0500

    Write specs for registeration and login successful response.

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -5,7 +5,10 @@ RSpec.describe User, type: :feature do
   describe "Controller Requests" do
 
     context 'Register and Login of Manager' do
-      let(:user) { described_class.create(role: :manager, name: 'John Doe', email: 'john.doe@email.com', password: '123456', phone_number: '123-456-7890') }
+      let :manager_data do
+        { role: :manager, name: 'John Doe', email: 'john.doe@email.com', password: '123456', phone_number: '123-456-7890' }
+      end
+      let(:user) { described_class.create(manager_data) }
 
       let :visit_signup do
         visit '/'
@@ -16,6 +19,17 @@ RSpec.describe User, type: :feature do
       it 'Signs up with test data' do
         visit_signup
         expect(page).to have_content('Sign up')
+
+        fill_in 'floatingName', with: manager_data[:name], exact: true
+        fill_in 'floatingPhoneNumber', with: manager_data[:phone_number], exact: true
+        fill_in 'floatingEmail', with: manager_data[:email], exact: true
+        fill_in 'floatingPassword', with: manager_data[:password], exact: true
+        fill_in 'floatingConfirmPassword', with: manager_data[:password], exact: true
+
+        click_button 'Sign up'
+        expect(page).to have_http_status(:success)
+        expect(current_path).to have_content('/projects')
+        expect(page).to have_button('Add New Project')
       end
 
       let :visit_sign_in do
@@ -30,12 +44,13 @@ RSpec.describe User, type: :feature do
 
         visit_sign_in
 
-        fill_in('Email', with: 'john.doe@email.com')
-        fill_in('Password', with: '123456')
+        fill_in('Email', with: manager_data[:email])
+        fill_in('Password', with: manager_data[:password])
         click_button('Login')
 
         expect(page).to have_http_status(:success)
         expect(current_path).to include('/projects')
+        expect(page).to have_button('Add New Project')
       end
     end
   end
