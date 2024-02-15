04213d57f6766a72b6eb1167c409ee953c96439c
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2024-02-15 18:32:32 +0500

    Create spec to not allow developer to create bug.

--- spec/requests/bug_spec.rb
+++ spec/requests/bug_spec.rb
@@ -12,18 +12,31 @@ RSpec.describe "Bugs", type: :request do
       it 'creates a bug' do
         bug_attrs = attributes_for(:bug, creator: qa)
         bug_attrs[:screenshot] = fixture_file_upload('spec/fixtures/images/example.png', 'image/png')
-        bug_attrs[:user_id] = developer.id
+        bug_attrs[:user_id] = developer.id  # todo - change this to developer_id if foreign key changes
 
         expect { post project_bugs_path(project), params: { bug: bug_attrs } }.to change(Bug, :count).by(1)
         expect(response).to redirect_to(project_path(project))
       end
     end
 
-
   end
 
   context 'when user is developer' do
+    let(:developer) { create :user, :developer }
+    before(:each) { sign_in developer }
+    let(:qa) { create :user, :QA }
+    let(:project) { create :project } # created by arbitrary manager
 
+    describe 'POST /projects/:project_id/bugs' do
+      it 'does not allow creating bug' do
+        bug_attrs = attributes_for :bug
+        bug_attrs[:screenshot] = fixture_file_upload('spec/fixtures/images/example.png', 'image/png')
+        bug_attrs[:user_id] = developer.id  # todo - change this to developer_id if foreign key changes
+
+        post project_bugs_path(project), params: { bug: bug_attrs }
+        expect(response).to have_http_status(:not_found)
+      end
+    end
   end
 
 end
