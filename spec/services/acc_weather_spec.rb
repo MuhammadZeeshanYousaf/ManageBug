6a21df9f947717c1fdcb41a879469ad0081a160f
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2024-02-19 13:51:22 +0500

    Write mock tests for daily forcasts.

--- spec/services/acc_weather_spec.rb
+++ spec/services/acc_weather_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 RSpec.describe AccuWeatherService do
   subject { described_class.new }
   let(:mocked_location) { fixture_services_file 'location.json' }
+  let(:mocked_error_response) { { error: 'mocked error occurs' } }
 
   describe '#get_location_key' do
     context 'when the API call is successful' do
@@ -39,12 +40,37 @@ RSpec.describe AccuWeatherService do
       end
     end
 
-    context 'when an error occurs on fetching location key' do
+    context 'when error occurs on fetching location key' do
       it 'returns nil' do
-        allow(subject).to receive(:get_location_key).and_return({ error: 'key fetching error' })
+        allow(subject).to receive(:get_location_key).and_return(mocked_error_response)
         mocked_key = subject.get_location_key
 
         expect(subject.current_conditions(mocked_key)).to be_nil
+      end
+    end
+
+  end
+
+  describe '#daily_forecasts' do
+    let(:mocked_daily_forcasts) { fixture_services_file 'daily_forcasts.json' }
+
+    context 'when location key is valid' do
+      it 'returns forcasts for 5 days' do
+        allow(subject).to receive(:get_location_key).and_return(mocked_location['Key'])
+        mocked_key = subject.get_location_key
+
+        allow_any_instance_of(RestClient::Resource).to receive(:get).and_return(mocked_daily_forcasts.to_json)
+
+        expect(subject.daily_forecasts(mocked_key)).to eq(mocked_daily_forcasts)
+      end
+    end
+
+    context 'when error occurs on fetching location key' do
+      it 'returns nil' do
+        allow(subject).to receive(:get_location_key).and_return(mocked_error_response)
+        mocked_key = subject.get_location_key
+
+        expect(subject.daily_forecasts(mocked_key)).to be_nil
       end
     end
 
